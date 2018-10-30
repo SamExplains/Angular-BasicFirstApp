20085df9b0a5dde586d8836aa919a6e284d39dd2
Sampuccino <Sampuccino@gmail.com>
2018-10-29 19:06:04 -0700

    A deeper look into angular and some features.

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: 'app-servers',
+  // template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  addNewServer = false;
+
+  constructor() {
+
+    setTimeout(() => {
+      this.addNewServer = true;
+    }, 2000);
+
+  }
+
+  ngOnInit() {
+  }
+
+}
